1a5fc8d246b5f797be65a4548ce01fedfb4ac0fe
BakaRyu1 <baka.ryu1@gmail.com>
2025-12-02 13:02:08 +0100

    Slightly more optimized approach for day 2 of 2025

--- 2025/Day02.cs
+++ 2025/Day02.cs
@@ -23,10 +23,18 @@ internal class Day02 : DayRunner<(long first, long last)[]>
         var sum = 0L;
         foreach (var (first, last) in data)
         {
-            for (var i = first; i <= last; ++i)
+            var lastArray = ToNumberArray(last);
+            for (var num = ToNumberArray(first); CompareNumbers(num, lastArray) <= 0; num = IncrementNumber(num, num.Length - 1))
             {
-                if (IsInvalidId_Part1(i))
+                if ((num.Length % 2) != 0)
                 {
+                    for (var i = 0; i < num.Length; ++i)
+                        num[i] = '9';
+                    continue;
+                }
+                if (IsInvalidId_Part1(num))
+                {
+                    var i = num.AsSpan().ToLong();
                     if (settings.Verbose)
                         Console.WriteLine($"Found {i} in range [{first}-{last}]");
                     sum += i;
@@ -41,10 +49,12 @@ internal class Day02 : DayRunner<(long first, long last)[]>
         var sum = 0L;
         foreach (var (first, last) in data)
         {
-            for (var i = first; i <= last; ++i)
+            var lastArray = ToNumberArray(last);
+            for (var num = ToNumberArray(first); CompareNumbers(num, lastArray) <= 0; num = IncrementNumber(num, num.Length - 1))
             {
-                if (IsInvalidId_Part2(i))
+                if (IsInvalidId_Part2(num))
                 {
+                    var i = num.AsSpan().ToLong();
                     if (settings.Verbose)
                         Console.WriteLine($"Found {i} in range [{first}-{last}]");
                     sum += i;
@@ -54,28 +64,26 @@ internal class Day02 : DayRunner<(long first, long last)[]>
         Console.WriteLine($"The sum of actual invalid ids is {sum}");
     }
 
-    private static bool IsInvalidId_Part1(long number)
+    private static bool IsInvalidId_Part1(ReadOnlySpan<char> str)
     {
-        var str = number.ToString();
         if ((str.Length % 2) != 0)
             return false;
         var halfLength = str.Length / 2;
-        return str.AsSpan(0, halfLength).SequenceEqual(str.AsSpan(halfLength));
+        return str[..halfLength].SequenceEqual(str[halfLength..]);
     }
 
-    private static bool IsInvalidId_Part2(long number)
+    private static bool IsInvalidId_Part2(ReadOnlySpan<char> str)
     {
-        var str = number.ToString();
         var maxPatternLen = str.Length / 2;
         for (var patternLen = 1; patternLen <= maxPatternLen; ++patternLen)
         {
             if ((str.Length % patternLen) != 0)
                 continue;
-            var pattern = str.AsSpan(0, patternLen);
+            var pattern = str[..patternLen];
             var patternMatch = true;
             for (var pos = patternLen; pos < str.Length; pos += patternLen)
             {
-                if (!pattern.SequenceEqual(str.AsSpan(pos, patternLen)))
+                if (!pattern.SequenceEqual(str.Slice(pos, patternLen)))
                 {
                     patternMatch = false;
                     break;
@@ -85,5 +93,32 @@ internal class Day02 : DayRunner<(long first, long last)[]>
                 return true;
         }
         return false;
+    }
+
+    private static char[] ToNumberArray(long number) => number.ToString().ToCharArray();
+
+    private static char[] IncrementNumber(char[] number, int pos = -1)
+    {
+        if (pos < 0)
+            pos = number.Length - 1;
+        var ch = ++number[pos];
+        if (ch <= '9')
+            return number;
+        number[pos] = '0';
+        if (pos == 0)
+            return ['1', .. Enumerable.Repeat('0', number.Length)];
+        return IncrementNumber(number, pos - 1);
+    }
+
+    private static int CompareNumbers(char[] a, char[] b)
+    {
+        if (a.Length != b.Length)
+            return a.Length - b.Length;
+        for (var i = 0; i < a.Length; ++i)
+        {
+            if (a[i] != b[i])
+                return a[i] - b[i];
+        }
+        return 0;
     }
 }
