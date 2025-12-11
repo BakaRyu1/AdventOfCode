1c714be2ee2c38888684557152a3c821f1c030fe
BakaRyu1 <baka.ryu1@gmail.com>
2025-12-11 16:57:32 +0100

    Solutions for day 10 part 1 and day 11 of 2025

---
+++ 2025/Day10.cs
@@ -0,0 +1,204 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2025;
+internal partial class Day10 : DayRunner<Day10.Machine[]>
+{
+    public struct Button
+    {
+        public BitVector32 Lights;
+    }
+    public struct Machine
+    {
+        public BitVector32 Lights;
+        public int LightsCount;
+        public Button[] Buttons;
+        public int[] Joltages;
+    }
+
+    public override Machine[] Parse(FileReference file)
+    {
+        var machines = new List<Machine>();
+        foreach (var line in file.GetLines())
+        {
+            BitVector32? lights = null;
+            int lightsCount = 0;
+            var buttons = new List<Button>();
+            int[]? joltages = null;
+            foreach (var part in line.AsSpan().SplitAsStrings())
+            {
+                if (part.StartsWith('[') && part.EndsWith(']'))
+                {
+                    if (lights != null)
+                        throw new InvalidOperationException($"Lights diagram is already defined: {part}");
+                    var bools = part.Skip(1).Take(part.Length - 2).Select(ch => ch switch
+                    {
+                        '.' => false,
+                        '#' => true,
+                        _ => throw new InvalidOperationException($"Unknown light state: {ch}")
+                    }).ToArray();
+                    if (bools.Length > 16)
+                        throw new InvalidOperationException("This algorithm does not support more than 16 lights");
+                    lights = IntsToVector(bools.Index().Where(tuple => tuple.Item).Select(tuple => tuple.Index));
+                    lightsCount = bools.Length;
+                }
+                else if (part.StartsWith('(') && part.EndsWith(')'))
+                {
+                    buttons.Add(new() { Lights = IntsToVector(part.AsSpan(1..^1).SplitInts(',')) });
+                }
+                else if (part.StartsWith('{') && part.EndsWith('}'))
+                {
+                    if (joltages != null)
+                        throw new InvalidOperationException($"Joltages are already defined: {part}");
+                    joltages = [.. part.AsSpan(1..^1).SplitInts(',')];
+                }
+                else
+                    throw new InvalidOperationException($"Unknown part type: {part}");
+            }
+            if (lights == null)
+                throw new InvalidOperationException("No lights diagram defined.");
+            if (buttons.Count == 0)
+                throw new InvalidOperationException("No buttons defined.");
+            if (joltages == null)
+                throw new InvalidOperationException("No joltages defined.");
+            machines.Add(new()
+            {
+                Lights = lights.Value,
+                LightsCount = lightsCount,
+                Buttons = [.. buttons],
+                Joltages = joltages
+            });
+        }
+        return [.. machines];
+    }
+
+    public override void Part1(Machine[] data, RunSettings settings)
+    {
+        var sum = 0;
+        foreach (var (i, machine) in data.Index())
+        {
+            int[]? bestButtons = null;
+            int bestButtonsCount = int.MaxValue;
+            foreach (var (buttons, state) in GenerateButtons(machine.Buttons, new()))
+            {
+                if (state.Equals(machine.Lights))
+                {
+                    var count = buttons.Aggregate((a, b) => a + b);
+                    if (count < bestButtonsCount)
+                    {
+                        bestButtonsCount = count;
+                        bestButtons = buttons;
+                    }
+                }
+            }
+            if (settings.Verbose)
+                Console.WriteLine($"Machine {i}: {(bestButtons != null ? string.Join(',', bestButtons) : "no solution found.")}");
+            sum += bestButtonsCount;
+        }
+        Console.WriteLine($"Minimum button presses required is {sum}");
+    }
+
+    public override void Part2(Machine[] data, RunSettings settings)
+    {
+        /*
+        var sum = 0;
+        foreach (var (i, machine) in data.Index())
+        {
+            int[] bestSequence = [];
+            int bestSequenceLen = int.MaxValue;
+            var queue = new Queue<(int[], int[])>();
+            queue.Enqueue(([..Enumerable.Repeat(0, machine.Buttons.Length)], [.. Enumerable.Repeat(0, machine.LightsCount)]));
+            //var visited = new Dictionary<BitVector32, int>();
+            
+            while (queue.Count > 0)
+            {
+                var (buttons, joltages) = queue.Dequeue();
+                if (Enumerable.Range(0, machine.LightsCount).All(i => joltages[i] == machine.Joltages[i]))
+                {
+                    bestSequenceLen = buttons.Length;
+                    bestSequence = buttons;
+                    continue;
+                }
+                //if (visited.TryGetValue(lightState, out var len) && len < buttons.Length)
+                //    continue;
+                //visited[lightState] = buttons.Length;
+                foreach (var (j, button) in machine.Buttons.Index())
+                {
+                    //if (!IsRelated(button, machine.Lights, lightState))
+                    //    continue;
+                    int[] newJoltages = UpdateJoltages(joltages, button);
+                    if (!IsJoltageValid(newJoltages, machine))
+                        continue;
+                    queue.Enqueue(([.. buttons, j], newJoltages));
+                }
+            }
+            if (settings.Verbose)
+                Console.WriteLine($"Machine {i}: {string.Join(',', bestSequence)}");
+            sum += bestSequenceLen;
+        }
+        Console.WriteLine($"Minimum button presses required is {sum}");
+        */
+    }
+
+    private static BitVector32 IntsToVector(IEnumerable<int> ints)
+    {
+        var vector = new BitVector32();
+        foreach (var i in ints)
+            vector[1 << i] = true;
+        return vector;
+    }
+
+    private static int[] CalculateJoltages(Machine machine, int[] buttons)
+    {
+        int[] joltages = [.. Enumerable.Repeat(0, machine.LightsCount)];
+        for (var i = 0; i < buttons.Length; ++i)
+        {
+            var button = machine.Buttons[i];
+            for (var j = 0; j < joltages.Length; ++j)
+            {
+                if (button.Lights[1 << j])
+                    joltages[j] += buttons[i];
+            }
+        }
+        return joltages;
+    }
+    private static int[] UpdateJoltages(int[] joltages, Button button)
+    {
+        int[] newJoltages = [.. joltages];
+        for (var i = 0; i < joltages.Length; ++i)
+        {
+            if (button.Lights[1 << i])
+                ++newJoltages[i];
+        }
+        return newJoltages;
+    }
+    private static int[] UpdateJoltages(int[] joltages, int[] otherJoltages)
+    {
+        int[] newJoltages = [.. joltages];
+        for (var i = 0; i < joltages.Length; ++i)
+            newJoltages[i] += otherJoltages[i];
+        return newJoltages;
+    }
+    private static bool IsJoltageValid(int[] joltages, Machine machine)
+        => Enumerable.Range(0, machine.LightsCount).All(i => joltages[i] <= machine.Joltages[i]);
+
+    public static IEnumerable<(int[], BitVector32)> GenerateButtons(IEnumerable<Button> buttons, BitVector32 initialState)
+    {
+        if (!buttons.Any())
+        {
+            yield return ([], initialState);
+            yield break;
+        }
+        foreach (var (subSequence, state) in GenerateButtons(buttons.Skip(1), initialState))
+            yield return ([0, .. subSequence], state);
+        var activeState = new BitVector32(initialState.Data ^ buttons.First().Lights.Data);
+        foreach (var (subSequence, state) in GenerateButtons(buttons.Skip(1), activeState))
+            yield return ([1, .. subSequence], state);
+    }
+}
---
+++ 2025/Day11.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2025;
+internal class Day11 : DayRunner<Day11.Device[]>
+{
+    public struct Device()
+    {
+        public string Name = "";
+        public List<string> Outputs = [];
+    }
+    public override Device[] Parse(FileReference file)
+    {
+        var devices = new List<Device>();
+        foreach (var line in file.GetLines())
+        {
+            var pos = line.IndexOf(':');
+            if (pos < 0)
+                throw new InvalidOperationException($"Invalid device specification: {line}");
+            devices.Add(new()
+            {
+                Name = line.AsSpan(0, pos).Trim().ToString(),
+                Outputs = [.. line.AsSpan(pos + 1).SplitAsStrings()]
+            });
+        }
+        return [.. devices];
+    }
+
+    public override void Part1(Device[] data, RunSettings settings)
+    {
+        var devices = data.ToDictionary(item => item.Name);
+        var count = CountPaths(devices, "you", "out", []);
+        Console.WriteLine($"There are {count} valid paths.");
+    }
+
+    public override void Part2(Device[] data, RunSettings settings)
+    {
+        var devices = data.ToDictionary(item => item.Name);
+        var svrFft = CountPaths(devices, "svr", "fft", []);
+        if (settings.Verbose)
+            Console.WriteLine($"svr->fft: {svrFft} paths");
+        var svrDac = CountPaths(devices, "svr", "dac", []);
+        if (settings.Verbose)
+            Console.WriteLine($"svr->dac: {svrDac} paths");
+        var dacFft = CountPaths(devices, "dac", "fft", []);
+        if (settings.Verbose)
+            Console.WriteLine($"dac->fft: {dacFft} paths");
+        var fftDac = CountPaths(devices, "fft", "dac", []);
+        if (settings.Verbose)
+            Console.WriteLine($"fft->dac: {fftDac} paths");
+        var fftOut = CountPaths(devices, "fft", "out", []);
+        if (settings.Verbose)
+            Console.WriteLine($"fft->out: {fftOut} paths");
+        var dacOut = CountPaths(devices, "dac", "out", []);
+        if (settings.Verbose)
+            Console.WriteLine($"dac->out: {dacOut} paths");
+        var count = (svrFft * fftDac * dacOut) + (svrDac * dacFft * fftOut);
+        Console.WriteLine($"There are {count} valid paths.");
+    }
+
+    public override void InitSettings(ref RunSettings settings)
+    {
+        settings.File1 ??= FileReference.Resource(typeof(Day11), settings.Example ? "day11-example1.txt" : "day11-input.txt");
+        settings.File2 ??= (settings.Example ? FileReference.Resource(typeof(Day11), "day11-example2.txt") : null);
+    }
+
+    private static long CountPaths(Dictionary<string, Device> devices, string from, string to, Dictionary<string, long> cache)
+    {
+        if (from == to)
+        {
+            return 1;
+        }    
+        if (cache.TryGetValue(from, out var cachedCount))
+            return cachedCount;
+        if (!devices.TryGetValue(from, out var device))
+            return 0;
+        var count = 0L;
+        foreach (var output in device.Outputs)
+        {
+            count += CountPaths(devices, output, to, cache);
+        }
+        cache[from] = count;
+        return count;
+    }
+}
