cedf8c103bc66f87b1e86abd1a22dc3158c82861
BakaRyu1 <baka.ryu1@gmail.com>
2025-12-12 23:36:32 +0100

    Solutions for days 24-25 of 2015

---
+++ 2015/Day24.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode._2015;
+internal class Day24 : DayRunner<int[]>
+{
+    public override int[] Parse(FileReference file)
+    {
+        var packages = new List<int>();
+        foreach (var line in file.GetLines())
+            packages.Add(line.AsSpan().ToInt());
+        packages.Sort();
+        packages.Reverse();
+        return [.. packages];
+    }
+
+    public override void Part1(int[] data, RunSettings settings)
+    {
+        var totalWeight = data.Aggregate((a, b) => a + b);
+        var groupWeight = totalWeight / 3;
+        if (settings.Verbose)
+            Console.WriteLine($"Group weight is {groupWeight}");
+        var bestGroup = data;
+        var bestQe = long.MaxValue;
+        foreach (var group1 in GetAllDispositions(data, groupWeight, []))
+        {
+            long? qe = null;
+            if (group1.Length > bestGroup.Length)
+                continue;
+            if (group1.Length == bestGroup.Length && (qe = QuantumEntanglement(group1)) > bestQe)
+                continue;
+            var remainingBoxes = data.Except(group1).ToList();
+            var group2 = GetAllDispositions(remainingBoxes, groupWeight, []).FirstOrDefault();
+            if (group2 == null)
+                continue;
+            bestGroup = group1;
+            bestQe = qe ??= QuantumEntanglement(group1);
+            if (settings.Verbose)
+            {
+                var group3 = remainingBoxes.Except(group2).ToArray();
+                Console.WriteLine($"{string.Join(" ", group1)} (QE={qe}); {string.Join(" ", group2)}; {string.Join(" ", group3)}");
+            }
+        }
+        Console.WriteLine($"Best group 1 is {string.Join(" ", bestGroup)}");
+        Console.WriteLine($"Quantum entanglement is {bestQe}");
+    }
+
+    public override void Part2(int[] data, RunSettings settings)
+    {
+        var totalWeight = data.Aggregate((a, b) => a + b);
+        var groupWeight = totalWeight / 4;
+        if (settings.Verbose)
+            Console.WriteLine($"Group weight is {groupWeight}");
+        var bestGroup = data;
+        var bestQe = long.MaxValue;
+        foreach (var group1 in GetAllDispositions(data, groupWeight, []))
+        {
+            long? qe = null;
+            if (group1.Length > bestGroup.Length)
+                continue;
+            if (group1.Length == bestGroup.Length && (qe = QuantumEntanglement(group1)) > bestQe)
+                continue;
+            var remainingBoxes = data.Except(group1).ToList();
+            int[]? group2 = null;
+            int[]? group3 = null;
+            int[]? group4 = null;
+            foreach (var possibleGroup2 in GetAllDispositions(remainingBoxes, groupWeight, []))
+            {
+                var remainingBoxes2 = remainingBoxes.Except(possibleGroup2).ToList();
+                group3 = GetAllDispositions(remainingBoxes2, groupWeight, []).FirstOrDefault();
+                if (group3 != null)
+                {
+                    group2 = possibleGroup2;
+                    if (settings.Verbose)
+                        group4 = [.. remainingBoxes2.Except(group3)];
+                    break;
+                }
+            }
+            if (group2 == null || group3 == null)
+                continue;
+            bestGroup = group1;
+            bestQe = qe ??= QuantumEntanglement(group1);
+            if (settings.Verbose)
+                Console.WriteLine($"{string.Join(" ", group1)} (QE={qe}); {string.Join(" ", group2)}; {string.Join(" ", group3)}; {string.Join(" ", group4!)}");
+        }
+        Console.WriteLine($"Best group 1 is {string.Join(" ", bestGroup)}");
+        Console.WriteLine($"Quantum entanglement is {bestQe}");
+    }
+
+    private static long QuantumEntanglement(IEnumerable<int> packages) => packages.Aggregate(1L, (a, b) => a * b);
+
+    private static IEnumerable<int[]> GetAllDispositions(IEnumerable<int> packages, int maxWeight, Dictionary<(int, int), int[][]> cache)
+    {
+        if (maxWeight == 0)
+        {
+            yield return [];
+            yield break;
+        }
+        if (cache.TryGetValue((packages.Count(), maxWeight), out var cachedSequences))
+        {
+            foreach (var sequence in cachedSequences)
+                yield return sequence;
+            yield break;
+        }
+        var sequences = new List<int[]>();
+        foreach (var (i, package) in packages.Index())
+        {
+            foreach (var subSequence in GetAllDispositions(packages.Skip(i + 1), maxWeight - package, cache))
+            {
+                int[] newSequence = [package, .. subSequence];
+                yield return newSequence;
+                sequences.Add(newSequence);
+            }
+        }
+        cache[(packages.Count(), maxWeight)] = [.. sequences];
+    }
+}
---
+++ 2015/Day25.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode.Utils;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2015;
+internal partial class Day25 : DayRunner<(int row, int column)>
+{
+    public override (int row, int column) Parse(FileReference file)
+    {
+        var text = file.GetText();
+        var match = TextPattern().Match(text);
+        if (!match.Success)
+            throw new InvalidOperationException($"Invalid input: {text}");
+        var row = match.Groups["row"].ValueSpan.ToInt();
+        var column = match.Groups["column"].ValueSpan.ToInt();
+        return (row, column);
+    }
+
+    public override void Part1((int row, int column) data, RunSettings settings)
+    {
+        foreach (var (pos, code) in GeneratePositions().Zip(GenerateCodes()))
+        {
+            if ((pos.X + 1) == data.column && (pos.Y + 1) == data.row)
+            {
+                Console.WriteLine($"The code at row {data.row} column {data.column} is {code}");
+                break;
+            }
+        }
+    }
+
+    public override void Part2((int row, int column) data, RunSettings settings)
+    {
+        Console.WriteLine("Merry Christmas!");
+    }
+
+    public override void InitSettings(ref RunSettings settings)
+    {
+        settings.File1 ??= FileReference.Resource(typeof(Day25), "day25-input.txt");
+    }
+
+    [GeneratedRegex(@"^To continue, please consult the code grid in the manual\.  Enter the code at row (?<row>\d+), column (?<column>\d+)\.$")]
+    private static partial Regex TextPattern();
+
+    private static IEnumerable<long> GenerateCodes()
+    {
+        long current = 20151125;
+        while (true)
+        {
+            yield return current;
+            current = (current * 252533) % 33554393;
+        }
+    }
+
+    private static IEnumerable<Position> GeneratePositions()
+    {
+        var x = 0;
+        var y = 0;
+        while (true)
+        {
+            yield return (x, y);
+            ++x;
+            --y;
+            if (y < 0)
+            {
+                y = x;
+                x = 0;
+            }
+        }
+    }
+}
